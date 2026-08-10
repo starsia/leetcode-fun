47d1c66cad47eb63e805ce4cc36fa91ffdf57678
Sia Zhong Qing <starsia93@gmail.com>
2026-08-10 22:14:37 +0800

    Use more efficient boundary check

--- graphs/695.Max-Area-of-Island/max-area-of-island.py
+++ graphs/695.Max-Area-of-Island/max-area-of-island.py
@@ -7,28 +7,31 @@ class Solution:
         if not grid:
             return 0
 
+        # instead of having a 'visited' set, we can modify the grid
+        # in-place to save some space complexity from O(island size) to O(1)
+
         rows, cols = len(grid), len(grid[0])
-	# we make an improvement by removing the 'visited' set and instead
-	# modify the grid in-place to save the space complexity from O(island size) to O(1)
+        # we also shift the direction list up to avoid creating at each bfs call
+        direction = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+
         def bfs(row, col) -> int:
             queue = collections.deque()
             
             queue.append((row, col))
             possible_largest = 1
-            island = [(row, col)]
 
-            direction = [(-1, 0), (1, 0), (0, 1), (0, -1)]
             while queue:
                 r, c = queue.popleft()
                 for dr, dc in direction:
                     row, col = dr + r, dc + c
 
-
-                    if row in range(rows) and col in range(cols) and grid[row][col] == 1:
+                    # we can use a direct comparision which is arithmetic instead of 
+                    # using the range method like row in range(rows) which creates
+                    # an object every time
+                    if 0 <= row < rows and 0 <= col < cols and grid[row][col] == 1:
                         grid[row][col] = 0
                         possible_largest += 1
                         queue.append((row, col))
-                        island.append((row, col))
                         
             return possible_largest
 
