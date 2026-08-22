ad6aa417c4f022b0ca51b758e796a93681847fd1
Sia Zhong Qing <starsia93@gmail.com>
2026-08-22 21:49:12 +0800

    Add permutation in string

--- sliding-window/3.Longest-Substring-Without-Repeating-Characters/longest-substring-without-repeating-characters.py
+++ sliding-window/3.Longest-Substring-Without-Repeating-Characters/longest-substring-without-repeating-characters.py
@@ -1,1 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        substring = set()
+        left = 0
+        max_length = 0
 
+        for right in range(len(s)):
+            while s[right] in substring:
+                substring.remove(s[left])
+                left += 1
+
+            substring.add(s[right])
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
+
+            
